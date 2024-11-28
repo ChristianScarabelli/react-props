4ca51ecba6dd33c01634513aceb02ede4ad210e9
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-28 21:22:07 +0100

    ADD: tags as a component & unique tags section

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import style from './Card.module.css'
 import placeHodelrImage from '../../assets/Img/600x400_placeholder.jpg'
 import Button from '../ui/Button/Button.jsx'
+import Tags from '../Tags/Tags.jsx'
+
 
 export default function Card({ title = '', image, content = '', tags = [] }) {
-    // const joinedTags = tags.join('')
 
     return (
         <>
@@ -14,18 +15,7 @@ export default function Card({ title = '', image, content = '', tags = [] }) {
                     </figure>
                     <div className={style.body}>
                         <h3 className={style.title}>{title}</h3>
-                        {tags.length > 0 && ( // Mostro i tag solo se esistono
-                            <div className={style.tags_container}>
-                                {tags.map((tag, index) => ( // genero i tag dinamicamente e aggiornabili tramite l'indice
-                                    <span
-                                        key={index}
-                                        className={`${style.tag} ${style[`tag_${tag.toLowerCase()}`]}`}
-                                    >
-                                        {tag}
-                                    </span>
-                                ))}
-                            </div>
-                        )}
+                        < Tags tags={tags} />
                         <p className={style.description}>{content}</p>
                         <Button />
                     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,49 @@
 import Card from './Card/Card.jsx'
 import { posts } from '../data/posts.js'
+import Tags from './Tags/Tags.jsx'
 
 export default function Main() {
+    const uniqueTags = [];
+    for (const post of posts) { // per ogni post dell'array di oggetti posts
+        for (const tag of post.tags) { // per ogni array di tags di ogni post
+            if (!uniqueTags.includes(tag)) {
+                uniqueTags.push(tag); // se il tag se non è già presente lo pusho
+            }
+        }
+    }
     return (
         <>
             <main>
-                <div className='container'>
-                    <h1 className='page-title'>Il mio blog</h1>
-                </div>
-                <div className='container'>
-                    {posts.length ? ( // Se ci sono post
-                        <div className="row">
-                            {posts.map((post) =>
-                                post.published ? ( // se published è settata a true,mostro solo i post pubblicati
-                                    <div key={post.id} className="col-6">
-                                        <Card
-                                            image={post.image}
-                                            title={post.title}
-                                            tags={post.tags}
-                                            content={post.content}
-                                        />
-                                    </div>
-                                ) : null // altrimenti non mostra nulla per i non pubblicati
-                            )}
-                        </div>
-                    ) : (
-                        <p>No posts available</p> // Se l'array `posts` è vuoto
-                    )}
-                </div>
+                <section className='posts_section'>
+                    <div className='container'>
+                        <h1 className='page-title'>Il mio blog</h1>
+                    </div>
+                    <div className='container'>
+                        {posts.length ? ( // Se ci sono post
+                            <div className="row">
+                                {posts.map((post) =>
+                                    post.published ? ( // se published è settata a true,mostro solo i post pubblicati
+                                        <div key={post.id} className="col-6">
+                                            <Card
+                                                image={post.image}
+                                                title={post.title}
+                                                tags={post.tags}
+                                                content={post.content}
+                                            />
+                                        </div>
+                                    ) : null // altrimenti non mostra nulla per i non pubblicati
+                                )}
+                            </div>
+                        ) : (
+                            <p>No posts available</p> // Se l'array `posts` è vuoto
+                        )}
+                    </div>
+                </section>
+                <section className='tags_stripe'>
+                    <div className='container'>
+                        < Tags tags={uniqueTags} />
+                    </div>
+                </section>
             </main>
         </>
     )
